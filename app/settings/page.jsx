d93a3083400ad1765eed0c94350103d47c96639b
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2025-12-04 22:05:32 +0100

    importing styles

--- app/settings/page.jsx
+++ app/settings/page.jsx
@@ -4,6 +4,7 @@ import NavBar from "@/components/NavBar";
 
 import { SettingsContext } from "@/context/SettingsContext";
 import { useContext, useEffect, useState } from "react";
+import "@/styles/navbar.css"
 
 const SettingsPage = () => {
   const {
--- app/stats/page.jsx
+++ app/stats/page.jsx
@@ -1,19 +1,30 @@
 "use client";
 
 import NavBar from "@/components/NavBar";
+import Button from "@/components/Button";
+import StatsList from "@/components/StatsList";
 import useTasksDetails from "@/hooks/useTasksDetails";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import "../page.css"
 
 const StatsPage = () => {
-  const { tasksDetails, getDailyTasks, getMonthlyTasks, getYearlyTasks } = useTasksDetails();
-
-  console.log(getDailyTasks())
-  console.log(getMonthlyTasks())
-  console.log(getYearlyTasks())
+  const {
+    tasksDetails,
+    getMinutesFocused,
+  } = useTasksDetails();
+  const [statsFormat, setStatsFormat] = useState("day");
 
   return (
     <div>
       <NavBar></NavBar>
+      <div>
+        <Button text="day" onClick={() => setStatsFormat("day")} />
+        <Button text="month" onClick={() => setStatsFormat("month")} />
+        <Button text="year" onClick={() => setStatsFormat("year")} />
+      </div>
+      <div className="">
+          <StatsList stats={tasksDetails} />
+      </div>
     </div>
   );
 };
