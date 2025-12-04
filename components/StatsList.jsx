4a64b58157265e95e1e717b2942176dc11b14bec
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2025-12-04 22:08:14 +0100

    rendering stats and pagination

---
+++ components/StatsList.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
+import "@/styles/stats.css"
+
+const StatsList = ({ stats }) => {
+  const [itemOffset, setItemOffset] = useState(0);
+  const pageSize = 2;
+
+  const endOffset = itemOffset + pageSize;
+
+  const pageCount = Math.ceil(stats.length / pageSize);
+  const current = stats.slice(itemOffset, endOffset);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * pageSize) % stats.length;
+    setItemOffset(newOffset);
+  };
+
+//   console.log(current)
+
+  return (
+    <div>
+      <ul>
+        {current.map(i => (
+          <li>{i.taskName}, {i.date}</li>
+        ))}
+      </ul>
+
+      <ReactPaginate
+        breakLabel="..."
+        previousLabel={"◀"}
+        nextLabel={"▶"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+      />
+    </div>
+  );
+};
+
+export default StatsList;
