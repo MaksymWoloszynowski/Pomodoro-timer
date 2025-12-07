8ee2d6c4576bc3838942c350de3157e5ec176ef3
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2025-12-07 16:49:59 +0100

    added new settings layout

--- app/settings/page.jsx
+++ app/settings/page.jsx
@@ -1,6 +1,9 @@
 "use client";
 
 import NavBar from "@/components/NavBar";
+import SoundSelect from "@/components/SoundSelect";
+import VolumeSlider from "@/components/VolumeSlider";
+import ColorTheme from "@/components/ColorTheme";
 
 import { SettingsContext } from "@/context/SettingsContext";
 import { useContext, useEffect, useState } from "react";
@@ -9,41 +12,54 @@ const SettingsPage = () => {
   const {
     workMinutes,
     breakMinutes,
-    theme,
     setWorkMinutes,
     setBreakMinutes,
-    setTheme,
   } = useContext(SettingsContext);
 
   const [workTime, setWorkTime] = useState(workMinutes);
   const [breakTime, setBreakTime] = useState(breakMinutes);
-  //   const [theme, setTheme] = useState(theme)
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     setWorkMinutes(Number(workTime));
     setBreakMinutes(Number(breakTime));
-
-    console.log("changed");
   };
 
   return (
     <div>
       <NavBar></NavBar>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          value={workTime}
-          onChange={(e) => setWorkTime(e.target.value)}
-        />
-        <input
-          type="number"
-          value={breakTime}
-          onChange={(e) => setBreakTime(e.target.value)}
-        />
-        <button type="submit">Save</button>
-      </form>
+      <div className="settings-wrapper">
+        <div className="settings-section timer-section">
+          <p className="settings-section-title">Timer</p>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="timer-input"
+              type="number"
+              value={workTime}
+              onChange={(e) => setWorkTime(e.target.value)}
+            />
+            <input
+              className="timer-input"
+              type="number"
+              value={breakTime}
+              onChange={(e) => setBreakTime(e.target.value)}
+            />
+            <button type="submit" className="timer-save-btn">
+              Save
+            </button>
+          </form>
+        </div>
+        <div className="settings-section theme-section">
+          <p className="settings-section-title">Theme</p>
+          <ColorTheme />
+        </div>
+        <div className="settings-section sound-section">
+          <p className="settings-section-title">Sound</p>
+          <SoundSelect className={"sound-select"}/>
+          <VolumeSlider />
+        </div>
+      </div>
     </div>
   );
 };
