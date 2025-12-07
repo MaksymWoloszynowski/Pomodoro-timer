5724afed55e16168e57bb6a53318d36392f64bf5
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2025-12-07 16:50:50 +0100

    new component for theme choosing

---
+++ components/ColorTheme.jsx
@@ -0,0 +1,53 @@
+import { SettingsContext } from "@/context/SettingsContext";
+import React, { useContext, useEffect } from "react";
+
+const pomodoroColors = ["1", "2", "3", "4", "5"];
+const breakColors = ["1", "2", "3", "4", "5"];
+
+const ColorTheme = () => {
+  const { workTheme, breakTheme, setWorkTheme, setBreakTheme } = useContext(SettingsContext);
+
+  const handlePomodoroClick = (idx) => {
+    const newTheme = `pomodoro-${idx + 1}`
+    setWorkTheme(newTheme);
+    localStorage.setItem("workTheme", newTheme)
+  };
+
+  const handleBreakClick = (idx) => {
+    const newTheme = `break-${idx + 1}`
+    setBreakTheme(`break-${idx + 1}`);
+    localStorage.setItem("breakTheme", newTheme)
+  };
+
+  return (
+    <div className="color-theme">
+      <div className="color-section">
+        <p>Pomodoro color</p>
+        <div className="boxes">
+          {pomodoroColors.map((_, idx) => (
+            <div
+              key={idx}
+              className={`box pomodoro-${idx + 1}`}
+              onClick={() => handlePomodoroClick(idx)}
+            ></div>
+          ))}
+        </div>
+      </div>
+
+      <div className="color-section">
+        <p>Break color</p>
+        <div className="boxes">
+          {breakColors.map((_, idx) => (
+            <div
+              key={idx}
+              className={`box break-${idx + 1}`}
+              onClick={() => handleBreakClick(idx)}
+            ></div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ColorTheme;
