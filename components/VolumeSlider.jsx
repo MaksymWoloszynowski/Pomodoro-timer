0f4b8f81e43b0716ee8592e161749a3b0f2a8166
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2025-12-07 16:52:23 +0100

    new volume slider component

---
+++ components/VolumeSlider.jsx
@@ -0,0 +1,23 @@
+import { SettingsContext } from "@/context/SettingsContext";
+import { useContext, useState } from "react";
+
+const VolumeSlider = () => {
+  const { alarmVolume, setAlarmVolume } = useContext(SettingsContext);
+
+  return (
+    <div className="volume-slider">
+      <p>Volume: </p>
+      <input
+        type="range"
+        min={0}
+        max={100}
+        step={1}
+        value={alarmVolume}
+        onChange={(e) => setAlarmVolume(Number(e.target.value))}
+      />
+      <p>{alarmVolume}%</p>
+    </div>
+  );
+};
+
+export default VolumeSlider;
