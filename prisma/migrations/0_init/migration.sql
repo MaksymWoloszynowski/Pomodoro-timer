d55c6ede888b24781948dfb67e928b2ddf260ace
Maksym_Woloszynowski <m.woloszynowski.272@studms.ug.edu.pl>
2025-12-13 14:13:44 +0100

    prisma

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,9 +1,13 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
+
 -- CreateTable
 CREATE TABLE "Review" (
     "id" SERIAL NOT NULL,
-    "start" INTEGER NOT NULL,
+    "stars" INTEGER NOT NULL,
     "content" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
 );
+
